4ba21c8a1ca210fdee8cef4e3f2a8c0f8e923127
Justin Dennis <jxd8037@g.rit.edu>
2020-07-27 09:47:36 -0400

    Moved github pages files to the source directory to try and get them to load

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -20,7 +20,7 @@ export const app = new Vue({
         },
         DisplayClass: function(value){
             let file = new XMLHttpRequest();
-            file.open("GET", "../resources/" + value + ".json", false);
+            file.open("GET", "../" + value + ".json", false);
             
             file.onreadystatechange = () => {
                 if(file.readyState == 4){
