2011959943d56dbc3e2e80d2bb1f847a7e59d3ec
jksands <jksands83@gmail.com>
2020-11-03 22:27:24 -0500

    Material was duplicated in Kimmy's merge - it's gone now

--- VulkanEngine/VulkanEngine/EntityManager.cpp
+++ VulkanEngine/VulkanEngine/EntityManager.cpp
@@ -102,7 +102,6 @@ void EntityManager::LoadMaterials()
     materials.push_back(std::make_shared<Material>("shaders/vert.spv", "shaders/frag.spv", false, attributeDescriptions, bindingDescriptions, "textures/frog.jpg"));
     materials.push_back(std::make_shared<Material>("shaders/vert.spv", "shaders/frag.spv", false, attributeDescriptions, bindingDescriptions, "textures/room.png"));
     materials.push_back(std::make_shared<Material>("shaders/SkyVert.spv", "shaders/SkyFrag.spv", false, attributeDescriptions, bindingDescriptions, "textures/Skybox/", 'S'));
-    materials.push_back(std::make_shared<Material>("shaders/SkyVert.spv", "shaders/SkyFrag.spv", false, attributeDescriptions, bindingDescriptions, "textures/Skybox/", 'S'));
 
     //TODO: Find a better way of doing this that will work for multiple types of inputs
     std::vector<VkVertexInputAttributeDescription> attributeDescription(1);
